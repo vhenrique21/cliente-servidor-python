ed007971e8c704caec87d9024f458d5ebe5889cf
Vitorhpx <vitorhugoperles@gmail.com>
2019-10-28 20:37:35 -0300

    Adds working client with messages

--- client.py
+++ client.py
@@ -8,41 +8,34 @@ MAX_LINE = 256
 
 
 if len(sys.argv) > 1:
-  userInput = sys.argv[1]
-  
-  [address, portAndFile] = userInput.split(":",1)
-  [port,fileName] = portAndFile.split('/',1)
+    userInput = sys.argv[1]
 
-  regex = re.compile(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
-  result = regex.match(address)
-  if not result:
-      address = socket.gethostbyname(address)
-  if not (port and fileName and address):
-    print("Entrada invalida")
-  
-else:     
-  print('usage: simplex-talk host \n', file=sys.stderr)
-  exit(1)
+    [address, portAndFile] = userInput.split(":", 1)
+    [port, fileName] = portAndFile.split('/', 1)
+    name = address
+    port = int(port)
 
-hp = socket.gethostbyname(HOST)
-if not hp:
-  print('simplex-talk: unknowmn host: ' + hp, file=sys.stderr)  
-  exit(1)
+    regex = re.compile(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+    result = regex.match(address)
+
+    if not result:
+        address = socket.gethostbyname(address)
+
+    if not (port and fileName and address):
+        print("Entrada invalida")
+
+else:
+    print('usage: simplex-talk host \n', file=sys.stderr)
+    exit(1)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-  if not s:
-    print('simplex-talk: socket', file=sys.stderr)  
-  if not s.connect((hp, PORT)):
-    print('simplex-talk: connect', file=sys.stderr)  
+    if not s:
+        print('simplex-talk: socket', file=sys.stderr)
 
-  while True:
-    line = sys.stdin.readline(MAX_LINE)
-    line = line
-    print(len(line))
-    s.send(str.encode(line))
+    if not s.connect((address, port)):
+        print('simplex-talk: connect', file=sys.stderr)
+    print('connected')
 
-
-
-
-  # for line in sys.stdin:
-  #   s.send(str.encode(line))
+    message = "GET /{fileName} HTTP/1.1\nHost: {address}\n".format(
+        fileName=fileName, address=name)
+    s.send(str.encode(message))
